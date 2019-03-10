91ae7fe669a49bb0b6afa37d2b968bf25eca4fe9
Michael Yantosca <mike@archivarius.net>
2019-03-10 13:14:21 -0500

    Add population dump with MPIO.

--- nkmax.cpp
+++ nkmax.cpp
@@ -10,7 +10,13 @@ using namespace std::chrono;
 int main(int argc, char *argv[]) {
   int r, k;
   uint32_t a, i, n, m;
+  MPI_File popl_fp;
+  MPI_Offset popl_off = 0;
+  MPI_Request popl_req;
+  MPI_Status popl_status;
   time_point<system_clock, nanoseconds> tp_a = system_clock::now();
+  const char *fname = "popl.out";
+
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &r);
   MPI_Comm_size(MPI_COMM_WORLD, &k);
@@ -24,11 +30,22 @@ int main(int argc, char *argv[]) {
       a++;
       if (a < argc) sscanf(argv[a], "%u", &n);
     }
+    // Input filename argument.
+    if (!strcmp("-i", argv[a])) {
+      a++;
+      if (a < argc) fname = argv[a];
+    }
     a++;
   }
 
-  // Determine the local population, adding 1 in cases where n is not evenly divisible by k.
-  m = n / k + ((n % k) != 0);
+  // Determine the local population, spreading the remainder evenly over the nodes.
+  m = n / k + ((n % k) > r);
+  for (i = 0; i < r; i++) {
+    popl_off += n / k + ((n % k) > i);
+  }
+  popl_off *= sizeof(uint32_t);
+  MPI_Datatype filetype, contig;
+  MPI_Type_contiguous(m, MPI_UNSIGNED, &filetype);
 
   // Create a seeded PNRG based on the standard Mersenne Twister engine.
   unsigned seed = system_clock::now().time_since_epoch().count();
@@ -44,6 +61,10 @@ int main(int argc, char *argv[]) {
     local_max = M[i] > local_max ? M[i] : local_max;
   }
 
+  // Dump local population for post-mortem debugging.
+  MPI_File_open(MPI_COMM_WORLD, fname, MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &popl_fp);
+  MPI_File_set_view(popl_fp, popl_off, MPI_UNSIGNED, MPI_UNSIGNED, "native", MPI_INFO_NULL);
+  MPI_File_iwrite_at(popl_fp, 0, M, m, MPI_UNSIGNED, &popl_req);
   // Send local max to root and determine global max.
   MPI_Reduce(&local_max, &global_max, 1, MPI_UNSIGNED, MPI_MAX, 0, MPI_COMM_WORLD);
 
@@ -51,6 +72,10 @@ int main(int argc, char *argv[]) {
   if (r == 0) {
     std::cout << "max = " << global_max << std::endl;
   }
+
+  // Wait for population dump. Close dump file.
+  MPI_Wait(&popl_req, &popl_status);
+  MPI_File_close(&popl_fp);
 
   // Clean up.
   delete[] M;
