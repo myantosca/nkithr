22b369c0e4891dc323c8a112fddb3d062b2a700d
Michael Yantosca <mike@archivarius.net>
2019-03-11 23:29:50 -0500

    Removed trial arg.

--- nkith.sh
+++ nkith.sh
@@ -4,7 +4,6 @@
 #SBATCH -N 8
 #SBATCH --ntasks-per-node 4
 
-$t=$1
 export TS="$(date +%Y-%m-%d.%H.%M.%S)"
 export DOUT="./results/nkmax/${TS}"
 mkdir -p ${DOUT}
@@ -12,10 +11,10 @@ mkdir -p ${DOUT}
 for ((k = 1; k <= 32; k *= 2)); do
     for ((n = $[2**10]; n <= $[2**28]; n *=2)); do
 	i=$n
-	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.max.$k.$n.$t.log 2> /dev/null
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.max.$k.$n.log 2> /dev/null
 	i=$[$n/2]
-	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.med.$k.$n.$t.log 2> /dev/null
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.med.$k.$n.log 2> /dev/null
 	i=1
-	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.min.$k.$n.$t.log 2> /dev/null
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.min.$k.$n.log 2> /dev/null
     done
 done
--- nkmax.sh
+++ nkmax.sh
@@ -3,13 +3,13 @@
 #SBATCH -t 01:00:00
 #SBATCH -N 8
 #SBATCH --ntasks-per-node 4
-$t=$1
+
 export TS="$(date +%Y-%m-%d.%H.%M.%S)"
 export DOUT="./results/nkmax/${TS}"
 mkdir -p ${DOUT}
 
 for ((k = 1; k <= 32; k *= 2)); do
     for ((n = $[2**10]; n <= $[2**28]; n *= 2)); do
-	mpirun -np $k ./nkmax -- -n $n 1> ${DOUT}/nkmax.$k.$n.$t.log 2> /dev/null
+	mpirun -np $k ./nkmax -- -n $n 1> ${DOUT}/nkmax.$k.$n.log 2> /dev/null
     done
 done
