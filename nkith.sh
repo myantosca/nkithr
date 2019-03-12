a9cfc1eba3814e1dbfcc6832ee81b031318d8b7a
Michael Yantosca <mike@archivarius.net>
2019-03-11 23:08:38 -0500

    Fix issues in sbatch scripts.

--- nkith.sh
+++ nkith.sh
@@ -10,13 +10,13 @@ mkdir -p ${DOUT}
 
 for ((t = 1; t <= 10; t++)); do
     for ((k = 1; k <= 32; k *= 2)); do
-	for ((n = 1024; n <= 1073741824; n *=2)); do
-	    i=n
-	    mpirun -np $k ./nkith -n $n 1> ${DOUT}/nkith.max.$k.$n.$t.log 2> /dev/null
-	    i=n/2
-	    mpirun -np $k ./nkith -n $n 1> ${DOUT}/nkith.med.$k.$n.$t.log 2> /dev/null
+	for ((n = $[2**10]; n <= $[2**28]; n *=2)); do
+	    i=$n
+	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.max.$k.$n.$t.log 2> /dev/null
+	    i=$[$n/2]
+	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.med.$k.$n.$t.log 2> /dev/null
 	    i=1
-	    mpirun -np $k ./nkith -n $n 1> ${DOUT}/nkith.min.$k.$n.$t.log 2> /dev/null
+	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.min.$k.$n.$t.log 2> /dev/null
 	done
     done
 done
--- nkmax.sh
+++ nkmax.sh
@@ -10,8 +10,8 @@ mkdir -p ${DOUT}
 
 for ((t = 1; t <= 10; t++)); do
     for ((k = 1; k <= 32; k *= 2)); do
-	for ((n = 1024; n <= 1073741824; n *= 2)); do
-	    mpirun -np $k ./nkmax -n $n 1> ${DOUT}/nkmax.$k.$n.$t.log 2> /dev/null
+	for ((n = $[2**10]; n <= $[2**28]; n *= 2)); do
+	    mpirun -np $k ./nkmax -- -n $n 1> ${DOUT}/nkmax.$k.$n.$t.log 2> /dev/null
 	done
     done
 done
