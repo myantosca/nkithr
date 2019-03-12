9178bbe0e551a0a0d1fab3b52564b9279e2cefad
Michael Yantosca <mike@archivarius.net>
2019-03-11 23:22:56 -0500

    Removing trial loop to respect crill time limit. Added trial arg to batch scripts.

--- nkith.sh
+++ nkith.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #SBATCH -p cosc6326
-#SBATCH -t 02:00:00
+#SBATCH -t 01:00:00
 #SBATCH -N 8
 #SBATCH --ntasks-per-node 4
 
+$t=$1
 export TS="$(date +%Y-%m-%d.%H.%M.%S)"
 export DOUT="./results/nkmax/${TS}"
 mkdir -p ${DOUT}
 
-for ((t = 1; t <= 10; t++)); do
-    for ((k = 1; k <= 32; k *= 2)); do
-	for ((n = $[2**10]; n <= $[2**28]; n *=2)); do
-	    i=$n
-	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.max.$k.$n.$t.log 2> /dev/null
-	    i=$[$n/2]
-	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.med.$k.$n.$t.log 2> /dev/null
-	    i=1
-	    mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.min.$k.$n.$t.log 2> /dev/null
-	done
+for ((k = 1; k <= 32; k *= 2)); do
+    for ((n = $[2**10]; n <= $[2**28]; n *=2)); do
+	i=$n
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.max.$k.$n.$t.log 2> /dev/null
+	i=$[$n/2]
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.med.$k.$n.$t.log 2> /dev/null
+	i=1
+	mpirun -np $k ./nkith -- -n $n -i $i 1> ${DOUT}/nkith.min.$k.$n.$t.log 2> /dev/null
     done
 done
