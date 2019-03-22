e711b7b02d5a467831fdd2fd918b62812ef6055b
Michael Yantosca <mike@archivarius.net>
2019-03-21 20:30:51 -0500

    Added sbatch scripts for nkithr runs.

---
+++ nkithr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p cosc6326
+#SBATCH -t 01:00:00
+#SBATCH -N 8
+#SBATCH --ntasks-per-node 4
+
+export TS="$(date +%Y-%m-%d.%H.%M.%S)"
+export DOUT="./results/nkithr/${TS}"
+mkdir -p ${DOUT}
+
+for ((k = 1; k <= 32; k *= 2)); do
+    for ((n = $[2**10]; n <= $[2**28]; n *=2)); do
+	i=$n
+	mpirun -np $k ./nkithr -- -n $n -i $i 1> ${DOUT}/nkithr.max.$k.$n.log 2> /dev/null
+	i=$[$n/2]
+	mpirun -np $k ./nkithr -- -n $n -i $i 1> ${DOUT}/nkithr.med.$k.$n.log 2> /dev/null
+	i=1
+	mpirun -np $k ./nkithr -- -n $n -i $i 1> ${DOUT}/nkithr.min.$k.$n.log 2> /dev/null
+    done
+done
